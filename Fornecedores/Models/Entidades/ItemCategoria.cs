ee620e5af92e099cfc2edf445828503ce633c3e9
RodrigoLopes80 <drigo.plopes@gmail.com>
2021-05-21 01:57:42 -0300

    Atualizando projeto com novas bibliotecas e  _layout.cshtml e css

---
+++ Fornecedores/Models/Entidades/ItemCategoria.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Fornecedores.Models.Entidades
+{
+    [Table("ItemCategoria")]
+    public class ItemCategoria
+    {
+    
+        [Key]
+        public int CatId { get; set; }
+
+        public string CatNome { get; set; }
+
+       public virtual ICollection<Fornecedor> Fcats { get; set; }
+    }
+
+}
